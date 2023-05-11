755d1c7cec21c8b1e4e254a236eeaaa7f35060ba
hyperslab <silasjm6@gmail.com>
2023-05-11 14:48:40 -0400

    Make URLs more human-readable

--- fantasycalendar/urls.py
+++ fantasycalendar/urls.py
@@ -1,16 +1,20 @@
-from django.urls import path
+from django.shortcuts import redirect
+from django.urls import path, reverse
 from . import views
 
 
 app_name = 'fantasycalendar'
 urlpatterns = [
-    path("", views.WorldIndexView.as_view(), name="world-index"),
-    path("<int:pk>/", views.WorldDetailView.as_view(), name="world-detail"),
-    path("<int:world_key>/<int:pk>/", views.CalendarDetailView.as_view(), name="calendar-detail"),
-    path("new/", views.WorldCreateView.as_view(), name="world-create"),
-    path("<int:world_key>/new/", views.CalendarCreateView.as_view(), name="calendar-create"),
-    path("<int:world_key>/<int:calendar_key>/new/", views.TimeUnitCreateView.as_view(), name="time-unit-create"),
-    path("<int:pk>/edit/", views.WorldUpdateView.as_view(), name="world-update"),
-    path("<int:world_key>/<int:pk>/edit/", views.CalendarUpdateView.as_view(), name="calendar-update"),
-    path("<int:world_key>/<int:calendar_key>/<int:pk>/edit/", views.TimeUnitUpdateView.as_view(), name="time-unit-update"),
+    path("", lambda req: redirect(reverse('fantasycalendar:world-index'))),
+    path("worlds/", views.WorldIndexView.as_view(), name="world-index"),
+    path("worlds/<int:pk>/", views.WorldDetailView.as_view(), name="world-detail"),
+    path("worlds/<int:world_key>/calendars/<int:pk>/", views.CalendarDetailView.as_view(), name="calendar-detail"),
+    path("worlds/new/", views.WorldCreateView.as_view(), name="world-create"),
+    path("worlds/<int:world_key>/calendars/new/", views.CalendarCreateView.as_view(), name="calendar-create"),
+    path("worlds/<int:world_key>/calendars/<int:calendar_key>/time-units/new/", views.TimeUnitCreateView.as_view(),
+         name="time-unit-create"),
+    path("worlds/<int:pk>/edit/", views.WorldUpdateView.as_view(), name="world-update"),
+    path("worlds/<int:world_key>/calendars/<int:pk>/edit/", views.CalendarUpdateView.as_view(), name="calendar-update"),
+    path("worlds/<int:world_key>/calendars/<int:calendar_key>/time-units/<int:pk>/edit/",
+         views.TimeUnitUpdateView.as_view(), name="time-unit-update"),
 ]
