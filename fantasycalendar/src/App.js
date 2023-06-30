2cb2721f0662cf5ff415fefe13e325d4ff289254
hyperslab <silasjm6@gmail.com>
2023-06-30 14:56:58 -0400

    Add block to base.html that gives access to full page (below header) and make React calendar page use it

--- fantasycalendar/src/App.js
+++ fantasycalendar/src/App.js
@@ -1,8 +1,29 @@
 import React from 'react'
 
+function DateSquare() {
+    return (
+        <div class="grid-item">
+            date
+        </div>
+    )
+}
+
+function DateSquares() {
+    return (
+        <div class="grid-container">
+            <DateSquare />
+            <DateSquare />
+            <DateSquare />
+        </div>
+    )
+}
+
 function App() {
     return (
-        <div>React is working</div>
+        <div>
+            <h4>Here is the calendar:</h4>
+            <DateSquares />
+        </div>
     )
 }
 
