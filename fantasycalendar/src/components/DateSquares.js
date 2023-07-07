aad9e84b57ca57eddedd7e5844b9a44adffa0bb8
hyperslab <silasjm6@gmail.com>
2023-07-07 11:43:23 -0400

    Fix DateSquare receiving Calendar's time unit instead of its base time unit

--- fantasycalendar/src/components/DateSquares.js
+++ fantasycalendar/src/components/DateSquares.js
@@ -3,22 +3,28 @@ import axios from 'axios';
 import DateSquare from './DateSquare.js';
 
 export default function DateSquares({ timeUnit, iteration, baseUnitInstanceClickHandler }) {
-    const [baseUnits, setBaseUnits] = React.useState(null);
+    const [baseUnit, setBaseUnit] = React.useState(null);
+    const [baseUnitInstances, setBaseUnitInstances] = React.useState(null);
 
     React.useEffect(() => {
         axios.get('/fantasy-calendar/api/timeunitbaseinstances/?time_unit_id=' + timeUnit.id + '&iteration=' + iteration)
             .then(res => {
-                setBaseUnits(res.data);
+                setBaseUnitInstances(res.data);
+                if (res.data && res.data.length > 0)
+                    axios.get('/fantasy-calendar/api/timeunits/' + res.data[0].time_unit_id)
+                        .then(res => {
+                            setBaseUnit(res.data);
+                        });
             });
         return () => {
-            setBaseUnits(null);  // have to clear it here or the old squares don't go away
+            setBaseUnitInstances(null);  // have to clear it here or the old squares don't go away
         }
     }, [timeUnit, iteration]);
 
-    if (!baseUnits) return null;
+    if (!baseUnit || !baseUnitInstances) return null;
 
-    const squares = baseUnits.map(baseUnit =>
-        <DateSquare key={baseUnit.iteration} timeUnit={timeUnit} timeUnitInstance={baseUnit} baseUnitInstanceClickHandler={baseUnitInstanceClickHandler} />
+    const squares = baseUnitInstances.map(baseUnitInstance =>
+        <DateSquare key={baseUnitInstance.iteration} timeUnit={baseUnit} timeUnitInstance={baseUnitInstance} baseUnitInstanceClickHandler={baseUnitInstanceClickHandler} />
     );
 
     return (
