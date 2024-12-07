d649390c44fb4cf0640da35dd978ee9be0d08dc3
hyperslab <silasjm6@gmail.com>
2024-12-07 17:50:43 -0500

    Change Azure settings to point to Azure SQL instead of PostgreSQL

--- fcsite/azure_settings.py
+++ fcsite/azure_settings.py
@@ -22,14 +22,15 @@ MIDDLEWARE = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
+conn_str = os.environ['AZURE_SQL_CONNECTIONSTRING']
+conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(';')}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': conn_str_params['dbname'],
-        'HOST': conn_str_params['host'],
-        'USER': conn_str_params['user'],
-        'PASSWORD': conn_str_params['password'],
+        'ENGINE': 'mssql',
+        'NAME': conn_str_params['Initial Catalog'],
+        'HOST': conn_str_params['Server'],
+        'USER': conn_str_params['User ID'],
+        'PASSWORD': conn_str_params['Password'],
     }
 }
+USE_TZ = False
