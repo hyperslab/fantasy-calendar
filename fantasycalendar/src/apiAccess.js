c603f3b21295a66bbc762f2bb2d6f1928a00a183
hyperslab <silasjm6@gmail.com>
2026-01-04 13:38:22 -0500

    Add caching to JS API access

--- fantasycalendar/src/apiAccess.js
+++ fantasycalendar/src/apiAccess.js
@@ -1,4 +1,8 @@
-import axios from 'axios';
+import Axios from 'axios';
+import { setupCache } from 'axios-cache-interceptor';
+
+const instance = Axios.create();
+const axios = setupCache(instance);
 
 const baseUrl = '/fantasy-calendar/api/';
 
