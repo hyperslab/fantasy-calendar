746e2f66cbcd3accb8a5768542889b596b2899af
hyperslab <silasjm6@gmail.com>
2026-04-18 11:27:37 -0400

    Fix DisplayUnitConfig update form so sub_unit_other_date_format looks at date formats for the sub unit if present instead of only looking at the base unit

--- fantasycalendar/views.py
+++ fantasycalendar/views.py
@@ -591,7 +591,10 @@ class DisplayUnitConfigUpdateView(UserPassesTestMixin, generic.UpdateView):
             time_unit_id=form.instance.time_unit_id)
         form.fields['header_other_date_format'].queryset = DateFormat.objects.filter(
             time_unit_id=form.instance.time_unit_id)
-        if form.instance.time_unit.base_unit:
+        if form.instance.sub_unit:
+            form.fields['sub_unit_other_date_format'].queryset = DateFormat.objects.filter(
+                time_unit_id=form.instance.sub_unit_id)
+        elif form.instance.time_unit.base_unit:
             form.fields['sub_unit_other_date_format'].queryset = DateFormat.objects.filter(
                 time_unit_id=form.instance.time_unit.base_unit_id)
         else:
